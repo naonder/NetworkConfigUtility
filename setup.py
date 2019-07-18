730423bd248abbae9b80962c242ab6317a5717bc
naonder <nate.a.onder@gmail.com>
2019-07-18 15:26:12 -0500

    Updated setup.py for long_description_content_type

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     version='1.0.0',
     description='Utility for pushing configuration data to network devices and retrieving state data',
     long_description=readme_file(),
+    long_description_content_type='text/markdown',
     author='naonder',
     author_email='nate.a.onder@gmail.com',
     license='MIT',
