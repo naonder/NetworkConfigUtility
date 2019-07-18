21ffdb93d56a1d4ce82140b72fd498232a20c080
naonder <nate.a.onder@gmail.com>
2019-07-18 14:55:06 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+def readme_file():
+    with open('README.md') as readme:
+        data = readme.read()
+    return data
+
+
+setup(
+    name='networkconfigutility',
+    version='1.0.0',
+    description='Utility for pushing configuration data to network devices and retrieving state data',
+    long_description=readme_file(),
+    author='naonder',
+    author_email='nate.a.onder@gmail.com',
+    license='MIT',
+    packages=['networkconfigutility'],
+    zip_safe=False,
+    install_requires=['nornir']
+)
